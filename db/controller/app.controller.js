87d94fe29272c22e23ee15341a0b59c57b51c754
stevenwu99 <stevenmaster@hotmail.com>
2023-06-28 08:21:51 +0100

    Task 5 Get/api/articles

--- db/controller/app.controller.js
+++ db/controller/app.controller.js
@@ -1,4 +1,4 @@
-const {selectAllTopics,selectArticleByArticleId } = require('../model/app.model')
+const {selectAllTopics,selectArticleByArticleId,selectAllArticles } = require('../model/app.model')
 
 //Task 2 Get All topics
 exports.getAllTopics = (req, res,next) => {
@@ -8,12 +8,24 @@ exports.getAllTopics = (req, res,next) => {
    .catch(next);
 };
 
-//GET /api/articles/:article_id
+//Task 4 GET /api/articles/:article_id
 exports.getArticleByArticleId = (req,res,next) => {
    const article_id  = req.params.article_id;
    selectArticleByArticleId (article_id)
    .then ((article) => {
-      res.status(200).send({article:article})
+      res.status(200).send({article:article[0]})
    })
    .catch (next);
 }
+
+
+//Task 5 CORE: GET /api/articles
+exports.getAllArticles = (req,res,next) => {
+   selectAllArticles().then((articles) => {  
+      res.status(200).send({articles});    
+   })
+   .catch(next);
+    
+
+}
+
--- db/model/app.model.js
+++ db/model/app.model.js
@@ -10,7 +10,7 @@ exports.selectAllTopics = () => {
     })
 }
 
-//GET /api/articles/:article_id
+//Task4 GET /api/articles/:article_id
 exports.selectArticleByArticleId = (article_id) => {
     const selectSQLStr = "SELECT * FROM articles WHERE article_id = $1 ;";
     return db
@@ -20,5 +20,18 @@ exports.selectArticleByArticleId = (article_id) => {
            return Promise.reject({status:404,msg:'Not found'})
          }
          return rows
+    })
+}
+
+//Task 5 CORE: GET /api/articles
+exports.selectAllArticles = () => {
+    const selectSQLStr = "SELECT a.author,a.title,a.article_id, a.topic,a.created_at,a.votes,a.article_img_url, " +
+                         "(SELECT count(b.article_id)::int FROM comments b WHERE b.article_id = a.article_id) AS comment_count " + 
+                         "FROM articles a "+
+                         "ORDER BY a.created_at DESC ;";
+    return db
+    .query(selectSQLStr)
+    .then(({rows}) => { 
+        return rows;
     })
 }
