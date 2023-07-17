925159ad50e083b24dc7bfba827ba9685b13f0d8
stevenwu99 <stevenmaster@hotmail.com>
2023-07-17 10:07:44 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const endpointsData = require('./endpoints.json')
-app.use(express.json());
 
+app.use(cors());
+app.use(express.json());
 
 
 //Require error handle
